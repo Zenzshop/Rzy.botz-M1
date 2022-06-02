ad90c03e4113281f698700033e40e4a0227b68b2
Zenzshop <106484301+Zenzshop@users.noreply.github.com>
2022-06-02 14:55:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -127,8 +127,8 @@ global.reloadHandler = function () {
     conn.off('group-update', conn.onGroupUpdate)
     conn.off('CB:action,,call', conn.onCall)
   }
-  conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject\n\n@desc'
-  conn.bye = 'Selamat tinggal @user'
+  conn.welcome = 'Hai kontol, @user!\nSelamat datang di grup @subject\n\n@desc'
+  conn.bye = 'wah beban kontol udah keluar jangan balik lagi yah @user'
   conn.spromote = '@user sekarang admin'
   conn.sdemote = '@user sekarang bukan admin'
   conn.handler = handler.handler
